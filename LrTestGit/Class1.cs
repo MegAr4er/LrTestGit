da52b9651e32764be73c2254dc751b43efd8d4f0
MegAr4er <ivansmollic@gmail.com>
2025-04-08 10:33:34 +0300

    Revert "ppp"
    This reverts commit 7014b31feb72e148d1b27432ecea2c5a68a396f4.

--- LrTestGit/Class1.cs
+++ LrTestGit/Class1.cs
@@ -3,15 +3,7 @@ namespace LrTestGit
 {
     public class Class1
     {
-        public void Sum()
-        {
-            int x = 9;
-            int y = 10;
-            int c = x + y;
-            //[[[[[[[[[[[[[
-        }
 
     }
-   
-   
+
 }
