7014b31feb72e148d1b27432ecea2c5a68a396f4
MegAr4er <ivansmollic@gmail.com>
2025-04-08 10:30:14 +0300

    ppp

--- LrTestGit/Class1.cs
+++ LrTestGit/Class1.cs
@@ -3,7 +3,15 @@ namespace LrTestGit
 {
     public class Class1
     {
+        public void Sum()
+        {
+            int x = 9;
+            int y = 10;
+            int c = x + y;
+            //[[[[[[[[[[[[[
+        }
 
     }
-
+   
+   
 }
